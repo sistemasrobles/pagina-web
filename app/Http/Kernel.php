e7580897a6972377b0bcbd420488b61ade548737
sistemasrobles <116920420+sistemasrobles@users.noreply.github.com>
2024-02-05 17:57:07 -0500

    as

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
 
-         'api_key' => \App\Http\Middleware\ApiKeyMiddleware::class,
+         //'api_key' => \App\Http\Middleware\ApiKeyMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
