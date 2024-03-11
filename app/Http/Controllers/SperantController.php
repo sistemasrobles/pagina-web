f0bf3bf9ab5239fec115bb176ea73b78babbf92a
sistemasrobles <116920420+sistemasrobles@users.noreply.github.com>
2024-03-11 10:25:16 -0500

    as

--- app/Http/Controllers/SperantController.php
+++ app/Http/Controllers/SperantController.php
@@ -73,7 +73,7 @@ class SperantController extends Controller
 
         $support = config('sperant.support_mail_1');
         //$support2 = config('sperant.support_mail_2');
-        $support2 = 'roblessistemas23@gmail.com';
+        $support2 = 'sistemasrobles23@gmail.com';
 
 
         Mail::to($support)->cc($support2)->send(new NotificacionSperant($e,$postData));
