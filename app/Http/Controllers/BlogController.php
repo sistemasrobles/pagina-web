396bb65e09ed7acc8ad826c39465eb9dbcd19f16
sistemasrobles <116920420+sistemasrobles@users.noreply.github.com>
2024-06-04 14:01:32 -0500

    as

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,7 @@ use Artesaos\SEOTools\Facades\JsonLdMulti;
 class BlogController extends Controller
 {
     public function blog(){
-        $blog=blog::orderBy('created_at','desc')
+        $blog=blog::where('activo','=',1)->orderBy('created_at','desc')
         ->paginate(8);
     
 
@@ -106,7 +106,7 @@ class BlogController extends Controller
 
 
 
-        $blogs=blog:::where('activo','=',1)->where('slug','<>',$slug)->orderBy('id','desc')->paginate(3);
+        $blogs=blog::where('activo','=',1)->where('slug','<>',$slug)->orderBy('id','desc')->paginate(3);
      
        
 
