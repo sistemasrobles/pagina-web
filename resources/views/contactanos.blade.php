9d141cdc2ddc36f18f892e1edfec1c879f9351ac
sistemasrobles <116920420+sistemasrobles@users.noreply.github.com>
2024-07-31 17:10:06 -0500

    as

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -94,7 +94,7 @@ background: linear-gradient(90deg, rgba(0, 150, 139, 0.3) 35%, rgba(0, 92, 83, 0
 
         <ul style="list-style: none;margin:0px;padding: 0px;">
             <li class="py-1"><i class="fa-solid fa-home me-2 color-green-2"></i>Edificio Capital Golf (Oficina 1801) - Av. Club Golf Los Incas 154 ; Santiago de Surco - Lima.</li>
-             <li class="py-1"><i class="fa-solid fa-phone me-2 color-green-2"></i>(+51) 983 479 617 / 905-436-588 </li>
+             <li class="py-1"><i class="fa-solid fa-phone me-2 color-green-2"></i>(+51) 983 479 617 / 924 266 566 </li>
               <li class="py-1" ><a href="javascript:void(0)" class="color-green-2" style="text-decoration:underline;"><i class="fa-regular fa-envelope me-2 color-green-2"></i>ventas@gruporobles.com.pe</a></li>
         </ul>
       
