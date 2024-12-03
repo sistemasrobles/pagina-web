8de639317745229bb53d9fc2a73ca9fd0d41164c
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2024-12-03 14:33:01 -0500

    add

--- resources/views/webinar.blade.php
+++ resources/views/webinar.blade.php
@@ -109,7 +109,7 @@ src="https://www.facebook.com/tr?id=841134490271081&ev=PageView&noscript=1"
 
          <img 
             class=" w-100 img-fluid d-none d-lg-block"
-            src="https://gruporobles.com.pe/storage/banners/webinar_diciembre_dekstop.jpg"
+            src="https://gruporobles.com.pe/storage/banners/webinar_diciembre_escritorio.jpg"
             alt="WEBINAR GRUPO ROBLES & YASIKOV"
             srcset=""
           />
