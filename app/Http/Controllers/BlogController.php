df22f98a1b08d629c64891a9f3ea0dcb91145834
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-01-07 16:07:15 -0500

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,7 +15,7 @@ class BlogController extends Controller
 {
     public function blog(){
         $blog=blog::where('activo','=',1)->orderBy('created_at','desc')
-        ->paginate(8);
+        ->paginate(9);
     
 
         SEOMeta::setTitle('Grupo Robles & Yasikov - blog');
