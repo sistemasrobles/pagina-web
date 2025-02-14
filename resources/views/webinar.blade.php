bf8d9740e4a2580c90163ae9839cc12fcc1e695f
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-02-14 16:43:48 -0500

    add

--- resources/views/webinar.blade.php
+++ resources/views/webinar.blade.php
@@ -270,7 +270,7 @@ src="https://www.facebook.com/tr?id=841134490271081&ev=PageView&noscript=1"
                 type="hidden"
                 id="proyecto"
                 name="proyecto"
-                value="31"
+                value="28"
               />
 
 
