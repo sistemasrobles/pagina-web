4a54021d6da1fb291f407e45a505ededffdc3267
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-02-15 10:19:25 -0500

    Update webinar.blade.php

--- resources/views/webinar.blade.php
+++ resources/views/webinar.blade.php
@@ -109,7 +109,7 @@ src="https://www.facebook.com/tr?id=841134490271081&ev=PageView&noscript=1"
 
          <img 
             class=" w-100 img-fluid d-none d-lg-block"
-            src="https://gruporobles.com.pe/storage/banners/webinar_feb-d.webp"
+            src="https://gruporobles.com.pe/storage/banners/webinar-feb-d.webp"
             alt="WEBINAR GRUPO ROBLES & YASIKOV"
             srcset=""
           />
