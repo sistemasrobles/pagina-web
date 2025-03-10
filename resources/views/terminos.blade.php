23bc55e9cbc0f7b58d2357df1dbb0016d53a1468
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-03-10 17:17:21 -0500

    Update terminos.blade.php

--- resources/views/terminos.blade.php
+++ resources/views/terminos.blade.php
@@ -44,8 +44,8 @@
         <h5 style="text-decoration: underline;">Política de protección de datos</h5>
 
         <p>A través de este apartado se señala el uso de datos en la relación entre este WEBSITE y los USUARIOS (visitantes e interesados) en los servicios que corresponden al mismo.
-        Con el fin de facilitar un desarrollo rápido y eficaz de la relación entre Grupo Robles & Yasikov y LOS USUARIOS, LA ASOCIACIÓN garantiza mediante este aviso, la privacidad de los servicios de acuerdo con las exigencias legales e informa acerca de su Política de Protección de Datos de carácter personal para que los USUARIOS determinen libre y voluntariamente si desean facilitar a LA ASOCIACIÓN su información personal a través de cualquier vía, en este caso mediante un formulario electrónico, en el WEBSITE. Dicha protección de datos seguirá en todo momento los lineamientos señalados por la Ley No. 29733, Ley de Protección de Datos Personales.
-        Los USUARIOS consentirán el tratamiento de dichos datos personales por LA ASOCIACIÓN en los términos siguientes:</p>
+        Con el fin de facilitar un desarrollo rápido y eficaz de la relación entre Grupo Robles & Yasikov y LOS USUARIOS, Grupo Robles y Yasikov en adelante Robles y Yasikov garantiza mediante este aviso, la privacidad de los servicios de acuerdo con las exigencias legales e informa acerca de su Política de Protección de Datos de carácter personal para que los USUARIOS determinen libre y voluntariamente si desean facilitar a Robles y Yasikov su información personal a través de cualquier vía, en este caso mediante un formulario electrónico, en el WEBSITE. Dicha protección de datos seguirá en todo momento los lineamientos señalados por la Ley No. 29733, Ley de Protección de Datos Personales.
+        Los USUARIOS consentirán el tratamiento de dichos datos personales por Robles y Yasikov en los términos siguientes:</p>
 
 
         <h6>1. Política de protección de datos</h6>
@@ -62,19 +62,19 @@
 
 
         <h6>3. Política de protección de usuarios</h6>
-        <p>Proporcionará a los USUARIOS los recursos técnicos adecuados para que, con carácter previo a la entrega de los datos personales, puedan acceder a este aviso o a cualquier otra información relevante, y puedan prestar su consentimiento a fin de que LA ASOCIACIÓN proceda al tratamiento adecuado de los datos personales de los USUARIOS.</p>
+        <p>Proporcionará a los USUARIOS los recursos técnicos adecuados para que, con carácter previo a la entrega de los datos personales, puedan acceder a este aviso o a cualquier otra información relevante, y puedan prestar su consentimiento a fin de que Robles y Yasikov proceda al tratamiento adecuado de los datos personales de los USUARIOS.</p>
 
 
  <h6>4. La recopilación y tratamiento de los datos personales</h6>
-<p>Como consecuencia de la navegación por las páginas del WEBSITE, de consulta, solicitud o suscripción en cualquier servicio ofrecido directamente por LA ASOCIACIÓN, tiene como finalidad, además de proporcionar al USUARIO una experiencia personalizada en la red, el mantenimiento de la relación eventualmente establecida entre ambos, así como la gestión, administración, prestación, ampliación y mejora de los servicios a los que el USUARIO decida suscribirse, darse de alta o utilizar. De igual forma, a la adecuación de dichos servicios a las preferencias y gustos de sus USUARIOS.</p>
+<p>Como consecuencia de la navegación por las páginas del WEBSITE, de consulta, solicitud o suscripción en cualquier servicio ofrecido directamente por Robles y Yasikov, tiene como finalidad, además de proporcionar al USUARIO una experiencia personalizada en la red, el mantenimiento de la relación eventualmente establecida entre ambos, así como la gestión, administración, prestación, ampliación y mejora de los servicios a los que el USUARIO decida suscribirse, darse de alta o utilizar. De igual forma, a la adecuación de dichos servicios a las preferencias y gustos de sus USUARIOS.</p>
 
 
 <p>Igualmente, el USUARIO es consciente, está informado y acepta que:</p>
 
 <ul>
     <li>Se utilizará la información de sus datos personales cuando así sea requerido por las autoridades administrativas competentes o por mandato judicial.</li>
-    <li>LA ASOCIACIÓN puede utilizar cookies (ficheros enviados a un navegador por medio de un servidor web para registrar las actividades del USUARIO en el WEBSITE) cuando un USUARIO navega por los sitios y páginas del WEBSITE.</li>
-    <li>Los servidores web de LA ASOCIACIÓN pueden registrar en determinados casos de manera automática la dirección IP y el nombre de dominio utilizados por el usuario, registrándose esta información en un fichero de actividad del servidor que permite el posterior procesamiento de los datos con el fin de obtener mediciones estadísticas que permitan conocer el número de impresiones de páginas, el número de visitas realizadas a los servicios web, entre otros.</li>
+    <li>Robles y Yasikov puede utilizar cookies (ficheros enviados a un navegador por medio de un servidor web para registrar las actividades del USUARIO en el WEBSITE) cuando un USUARIO navega por los sitios y páginas del WEBSITE.</li>
+    <li>Los servidores web de Robles y Yasikov pueden registrar en determinados casos de manera automática la dirección IP y el nombre de dominio utilizados por el usuario, registrándose esta información en un fichero de actividad del servidor que permite el posterior procesamiento de los datos con el fin de obtener mediciones estadísticas que permitan conocer el número de impresiones de páginas, el número de visitas realizadas a los servicios web, entre otros.</li>
     <li>Este sitio es medido y/o auditado con los alcances que ponen a disposición comScore y Google Analytcs, que pueden utilizar marcas en las páginas y cookies anónimas para analizar lo que sucede en nuestras páginas, de acuerdo con la política de privacidad disponible en sus respectivas páginas web.</li>
 </ul>
 
@@ -95,13 +95,13 @@
 
 
 <h6>2. Menores</h6>
-<p>Salvo todo lo anterior, los USUARIOS quedan advertidos de que LA ASOCIACIÓN no puede controlar que menores de edad hagan uso del WEBSITE y los Servicios. En ese sentido, LA ASOCIACIÓN, su Grupo y el Portal no asumen ninguna responsabilidad en caso algún menor de edad intente acceder al WEBSITE; en cuyo caso serán responsables los padres o tutores correspondientes, quienes serán los únicos encargados de controlar y asistir a los menores en la navegación en el WEBSITE, así como habilitar cualquiera de los mecanismos necesarios para que, en su caso, impidan el acceso de menores al WEBSITE y/o Servicios. LA ASOCIACIÓN no admitirá ningún reclamo en ese sentido.
-LA ASOCIACIÓN es responsable de la información que se pueda ingresar por USUARIOS menores de edad, y tomará todas las medidas necesarias para proteger la confidencialidad. Sin embargo, es consciente de que una seguridad absoluta en internet es inexistente.</p>
+<p>Salvo todo lo anterior, los USUARIOS quedan advertidos de que Robles y Yasikov no puede controlar que menores de edad hagan uso del WEBSITE y los Servicios. En ese sentido, Robles y Yasikov, su Grupo y el Portal no asumen ninguna responsabilidad en caso algún menor de edad intente acceder al WEBSITE; en cuyo caso serán responsables los padres o tutores correspondientes, quienes serán los únicos encargados de controlar y asistir a los menores en la navegación en el WEBSITE, así como habilitar cualquiera de los mecanismos necesarios para que, en su caso, impidan el acceso de menores al WEBSITE y/o Servicios. Robles y Yasikov no admitirá ningún reclamo en ese sentido.
+Robles y Yasikov es responsable de la información que se pueda ingresar por USUARIOS menores de edad, y tomará todas las medidas necesarias para proteger la confidencialidad. Sin embargo, es consciente de que una seguridad absoluta en internet es inexistente.</p>
 
 
  <h6>3. Varios</h6>
 <ul>
-    <li>Cambios en las condiciones de uso. LA ASOCIACIÓN se reserva el derecho de añadir o modificar la presente política de acuerdo a su criterio y/o para adaptarla a novedades o exigencias legales, así como a prácticas de la industria, teniendo en cuenta siempre los legítimos intereses del consumidor o del USUARIO. Ciertos servicios prestados en el WEBSITE pueden contener condiciones particulares con previsiones específicas en materia de protección de datos personales.</li>
+    <li>Cambios en las condiciones de uso. Robles y Yasikov se reserva el derecho de añadir o modificar la presente política de acuerdo a su criterio y/o para adaptarla a novedades o exigencias legales, así como a prácticas de la industria, teniendo en cuenta siempre los legítimos intereses del consumidor o del USUARIO. Ciertos servicios prestados en el WEBSITE pueden contener condiciones particulares con previsiones específicas en materia de protección de datos personales.</li>
     <li>Los USUARIOS deberán revisar este documento para ver si ha sufrido cambios, los que serán también debidamente anunciados.
 Nuestros precios y/o descuentos estan sujetos a cambios y variaciones.</li>
 </ul>
@@ -115,7 +115,7 @@ Nuestros precios y/o descuentos estan sujetos a cambios y variaciones.</li>
 
  <ul>
      <li>Estas CONDICIONES se rigen por la ley peruana.</li>
-      <li>Estos Términos serán aplicados e interpretados de acuerdo con la legislación peruana, y cualquier disputa que surja en su aplicación se verá únicamente ante los tribunales con jurisdicción en Lima, Perú. No obstante, esto no impedirá a LA ASOCIACIÓN el derecho a resolver cualquier litigio mediante arbitraje u otra jurisdicción competente.</li>
+      <li>Estos Términos serán aplicados e interpretados de acuerdo con la legislación peruana, y cualquier disputa que surja en su aplicación se verá únicamente ante los tribunales con jurisdicción en Lima, Perú. No obstante, esto no impedirá a Robles y Yasikov el derecho a resolver cualquier litigio mediante arbitraje u otra jurisdicción competente.</li>
  </ul>
 
 
