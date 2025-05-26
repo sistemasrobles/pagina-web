cf3c718dd05e5fd404daa0aff0689f3d26cb3b9c
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-05-26 16:48:06 -0500

    Update footer.blade.php

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -7,6 +7,13 @@
         <h4 class="mb-4">Contáctanos</h4>
         <div class="d-flex gap-4 flex-column">
           <span class="d-flex gap-2  align-items-center"><i class="fa-solid fa-phone"></i> (+51) {{$telefono_footer->getPhoneProyect(1)}} / 924-266-566</span>
+
+         
+
+          <span class="d-flex gap-2  align-items-center"><i class="fa-solid fa-phone"></i> Teléfono PostVenta : (+51) 947 278 914</span>
+
+
+
           <span class="d-flex gap-2 align-items-center"><i class="fa-regular fa-envelope"></i>ventas@gruporobles.com.pe</span>
         </div>
       </div>
