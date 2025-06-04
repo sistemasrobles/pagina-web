19b4a744918ab1a5ef5067620cbd439b670ec8ea
Grupo Robles & Yasikov <116920420+sistemasrobles@users.noreply.github.com>
2025-06-04 11:12:53 -0500

    Update PoryectoController.php

--- app/Http/Controllers/PoryectoController.php
+++ app/Http/Controllers/PoryectoController.php
@@ -51,7 +51,7 @@ class PoryectoController extends Controller
 
         }else{
 
-            return (isset($proyetoactual->wsp_bit))?$proyetoactual->wsp_bit:'https://goo.su/XFViR';
+            return (isset($proyetoactual->wsp_bit))?$proyetoactual->wsp_bit:'https://goo.su/mu6eJLo';
             //return 'https://bit.ly/RoblesYyasikov';
 
         }
